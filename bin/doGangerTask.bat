35ea57e77b7dfe6e454a6d966a4bd277aea57613
min-cimpress <mju@cimpress.com>
2016-10-24 19:14:46 +0900

    Update doGangerTask.bat

--- bin/doGangerTask.bat
+++ bin/doGangerTask.bat
@@ -1,3 +1,7 @@
-@echo off
+rem @echo off
 set /p pwd=< C:\bin\meta
 call runas /savecred /user:PRDKISMITPHO001\Administrator "%pwd%doGang.bat"
+
+rem robocopy pdf to archieve folder
+set /p jig=< C:\bin\jigmeta
+call runas /savecred /user:PRDKISMITPHO001\Administrator "robocopy %pwd% \\PRDKISMITPNC001\Archieve\%jig% out.pdf"
