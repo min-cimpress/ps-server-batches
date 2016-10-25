64abd760f8772cba87d02487e04fc43350b090a4
min-cimpress <mju@cimpress.com>
2016-10-25 15:48:48 +0900

    Update doGangerTask.bat

--- bin/doGangerTask.bat
+++ bin/doGangerTask.bat
@@ -5,3 +5,6 @@ call runas /savecred /user:PRDKISMITPHO001\Administrator "%pwd%doGang.bat"
 rem robocopy pdf to archieve folder
 set /p jig=< C:\bin\jigmeta
 call runas /savecred /user:PRDKISMITPHO001\Administrator "robocopy %pwd% \\PRDKISMITPNC001\Archieve\%jig% out.pdf"
+
+rem delete the origin out.pdf
+call runas /savecred /user:PRDKISMITPHO001\Administrator "del %pwd%out.pdf"
